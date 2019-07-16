e4298cf82e586885655d99589bf8f581d005699d
Grant Nestor <grantnestor@gmail.com>
2019-07-16 10:37:41 -0700

    Remove plotly-extension

--- packages/plotly-extension/src/lib.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'plotly.js/dist/plotly' {
-  export * from 'plotly.js';
-  export type Frame = { [key: string]: any };
-  export function addFrames(root: Plotly.Root, frames: Frame[]): Promise<void>;
-  export function animate(root: Plotly.Root): void;
-}
