7eb1df89ef7f1e8d3225d371f7f7c3240b3d7c00
Frédéric Collonval <fcollonval@gmail.com>
2022-11-14 17:33:04 +0100

    GeoJSON icon is not displayed any longer in the filebrowser

--- packages/geojson-extension/src/icons.ts
+++ packages/geojson-extension/src/icons.ts
@@ -1,7 +1,13 @@
 import { LabIcon } from '@jupyterlab/ui-components';
 import layersSvgstr from '../style/icons/layers-32px.svg';
+import mapSvgstr from '../style/icons/geojson.svg';
 
 export const layersIcon = new LabIcon({
-  name: 'jupyterlab-favorites:layers',
+  name: '@jupyterlab/geojson-extension:layers',
   svgstr: layersSvgstr,
+});
+
+export const mapIcon = new LabIcon({
+  name: '@jupyterlab/geojson-extension:map',
+  svgstr: mapSvgstr,
 });
