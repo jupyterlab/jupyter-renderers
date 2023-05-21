431597f5e95f306c62491ca26297e88aac1a9cb1
Frederic COLLONVAL <fcollonval@users.noreply.github.com>
2023-05-21 16:12:59 +0200

    add update script

---
+++ scripts/update-template.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# To be executed in the examples root folder
+git commit -am "Update extension template"
+
+for directory in ./packages/*/
+do
+    pushd ${directory}
+    copier update -o inline
+    git add --all .
+    git commit --amend -m "Update extension template"
+    popd
+done
+
+git reset HEAD~1
